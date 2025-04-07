6bd160e3a4b6ca0dc54817af0a90f0e25ad9e745
Louis Alaux <louis.alaux@gmail.com>
2025-04-07 09:46:25 +0200

    transisitons fonctionelles

--- src/layouts/home.js
+++ src/layouts/home.js
@@ -2,8 +2,10 @@ import React from 'react';
 import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import ButtonGroup from "../components/buttons/VerticalGroup";
+import { motion } from "framer-motion";
 import "./home.css";
 
+// Fond statique
 const BackgroundBox = styled(Box)({
   backgroundImage: `url(/test.jpg)`,
   backgroundSize: 'cover',
@@ -16,15 +18,23 @@ const BackgroundBox = styled(Box)({
 export default function MainLayout() {
   return (
     <BackgroundBox className="centered-container">
-      <div className="header-container">
-      <img
-          src="/guess_the_protocol.png"
-          alt="Guess the Protocol Logo"
-          className="logo-image"
-        />
-        <p className="subtitle">Le jeu de TC !</p>
-      </div>
-      <ButtonGroup />
+      {/* Contenu animé */}
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: -50 }}
+        transition={{ duration: 0.6 }}
+      >
+        <div className="header-container">
+          <img
+            src="/guess_the_protocol.png"
+            alt="Guess the Protocol Logo"
+            className="logo-image"
+          />
+          <p className="subtitle">Le jeu de TC !</p>
+        </div>
+        <ButtonGroup />
+      </motion.div>
     </BackgroundBox>
   );
 }
